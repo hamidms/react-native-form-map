42b1fa48658f049ac5c46ac3d4f12426406f7054
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-01-09 20:37:12 +0700

    Add Icon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator  } from "@react-navigation/bottom-tabs";
 
+import Icon from "react-native-vector-icons/MaterialIcons";
+
 import HomeScreen from "./screens/HomeScreen";
 import FormScreen from "./screens/FormScreen";
 import MapScreen from "./screens/MapScreen";
@@ -13,9 +15,33 @@ export default function MainContainer() {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-              <Tab.Screen name="Home" component={HomeScreen} />
-              <Tab.Screen name="Form" component={FormScreen} />
-              <Tab.Screen name="Map" component={MapScreen} />
+              <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Icon name="home" size={34} color={'black'} />
+                  ),
+                }}
+              />
+              <Tab.Screen
+                name="Form"
+                component={FormScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Icon name="article" size={34} color={'black'} />
+                  ),
+                }}
+              />
+              <Tab.Screen
+                name="Map"
+                component={MapScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Icon name="map" size={34} color={'black'} />
+                  ),
+                }}
+              />
             </Tab.Navigator>
         </NavigationContainer>        
       );
