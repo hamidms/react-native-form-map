45191716b1940589f1e7ae374b0211290099e1ad
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-01-09 19:14:56 +0700

    refactor

--- index.js
+++ index.js
@@ -3,13 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './src/App';
-// import CoreComponent from './src/CoreComponent';
-// import DesainTampilan from './src/DesainTampilan';
-// import StateProp from './src/StateProp';
-// import ContohForm from './src/ContohForm';
-// import TabNavigator from './src/navigation/TabNavigator';
-import MainContainer from './src/MainContainer';
+import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => MainContainer);
+AppRegistry.registerComponent(appName, () => App);
